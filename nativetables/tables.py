9d44521258d1f14e1898dfa28d457bbbca081f72
Chris Keele <dev@chriskeele.com>
2012-05-18 18:02:54 -0500

    More breakage while moving to django sessions
    Signed-off-by: Chris Keele <dev@chriskeele.com>

--- nativetables/tables.py
+++ nativetables/tables.py
@@ -10,34 +10,46 @@ class FeatureSet(list):
     def __unicode__(self):
         return mark_safe(''.join([item.__unicode__() for item in self]))
 
+class DatatableOptions(object):
+    def __init__(self, options=None):
+        self.name = getattr(options, 'name', 'datatable')
+        self.model = getattr(options, 'model', None)
+        
+class DatatableState(object):
+    def __init__(self, state=None):
+        self.search_param = getattr(state, 'search_param', '')
+        self.filter_values = getattr(state, 'filter_values', {})
+        self.ordering = getattr(state, 'ordering', {})
+        self.page_number = getattr(state, 'page_number', 1)
+        self.page = getattr(state, 'page', None)
+        # self.fields = getattr(options, 'fields', None)
+        # self.exclude = getattr(options, 'exclude', None)
+        # self.widgets = getattr(options, 'widgets', None)
+
 class DatatableMetaclass(type):
     def __new__(cls, name, bases, attrs):
-        # # If this isn't a subclass of Datatable, don't do anything special.
-        # try:
-        #     parents = [b for b in bases if issubclass(b, ModelForm)]
-        # except NameError:
-        #     # We are defining ModelForm itself.
-        #     parents = None
-        # new_class = super(ModelFormMetaclass, cls).__new__(cls, name, bases, attrs)
-        # if not parents:
-        #     return new_class
-        
-        attrs['features']=FeatureSet(())
-        attrs['filters']=FeatureSet(())
+        new_table = super(DatatableMetaclass, cls).__new__(cls, name, bases, attrs)
+        features = attrs.get('features', FeatureSet(()))
+        filters = attrs.get('filters', FeatureSet(()))
         for key, attr in attrs.items():
+            # print key, attr
             if isinstance(attr, BaseFeature):
-                # Populate a list of filters that were declared
+                # Populate a list of features that were declared
                 attr.set_name(key)
-                attrs['features'].insert(bisect(attrs['features'], attr), attr)
+                features.insert(bisect(features, attr), attr)
                 if isinstance(attr, BaseFilter):
-                    attrs['filters'].insert(bisect(attrs['filters'], attr), attr)
-        
-        return super(DatatableMetaclass, cls).__new__(cls, name, bases, attrs)
+                    filters.insert(bisect(filters, attr), attr)
+                    
+        new_table.features=features
+        new_table.filters=filters
+        new_table._meta = DatatableOptions(getattr(new_table, 'Meta', None))
+        new_table._state = DatatableState(getattr(new_table, 'Initial', None))
+        return new_table
 
 class BaseDatatable(Manager):
     def __init__(self, *args, **kwargs):
         super(BaseDatatable, self).__init__()
-        self.keys=['id','features','filters','searchable','filterable','orderable','paginate','paginator','per_page','state','initial']
+        self.keys=['id','features','filters','searches','orderings','pagination','searchable','orderable','paginate','paginator','state','initial']
         for key in self.keys:
             if not hasattr(self,key):
                 setattr(self,key,None)
@@ -46,23 +58,32 @@ class BaseDatatable(Manager):
                         keys=self.keys,
                         **{ key:getattr(self, key) for key in self.keys }
         )
-
+    def transform(self, **kwargs):
+        return self.get_query_set().transform(**kwargs)
+                
 class Datatable(BaseDatatable):
     __metaclass__ = DatatableMetaclass
-
+    def __init__(self, *args, **kwargs):
+        super(Datatable, self).__init__()
+        # Pin Datatable to associated model
+        self._meta.model.add_to_class(self._meta.name, self)
+        # Give datatable model info for abstraction
+        self.model = self._meta.model
+        
 class DataSet(QuerySet):
     ##########
     # Override base QuerySet methods to keep track of DataSet meta
     ##########
-    def __init__(self, keys, model=None, query=None, using=None, **kwargs):
+    def __init__(self, model, keys, query=None, using=None, **kwargs):
         super(DataSet, self).__init__(model, query, using)
+        self.model = model
         self.keys=keys
         for key in keys:
             setattr(self,key,kwargs[key])
     
     def __getitem__(self, k):
         result = super(DataSet, self).__getitem__(k)
-        return DataList(keys=self.keys, result=result, **{ key:getattr(self, key) for key in self.keys })
+        return DataList(model=self.model, keys=self.keys, result=result, **{ key:getattr(self, key) for key in self.keys })
     
     def _clone(self, klass=None, setup=False, **kwargs):
         if klass is None:
@@ -81,6 +102,11 @@ class DataSet(QuerySet):
     # End overrides
     ##########
     
+    # Modify datatable.state from a dict of options
+    def update_state(self, **kwargs):
+        
+    
+    # Apply pending changes to state
     def transform(self, **kwargs):
         search_param=kwargs.get('search_param',"")
         filter_values=kwargs.get('filter_values',{})
@@ -115,6 +141,7 @@ class DataSet(QuerySet):
         return self.filter(**search_args) if search_args else self
         
     def order(self, ordering):
+        print ordering
         for order_field, direction in ordering.iteritems():
             order_args = ("-" if direction=="desc" else "")+order_field
         return self.order_by(order_args) if order_args else self
@@ -127,8 +154,9 @@ class DataList(list):
     ##########
     ## Override base list methods to keep track of DataList meta
     ##########
-    def __init__(self, keys, result, **kwargs):
+    def __init__(self, model, keys, result, **kwargs):
         list.__init__(self, result)
+        self.model=model
         self.keys=keys
         for key in keys:
             setattr(self,key,kwargs[key])
--- nativetables/views.py
+++ nativetables/views.py
@@ -13,22 +13,22 @@ class DatatableMixin(object):
     datatable = None
     context_datatable_name = None
 
-    def get_tranformation_params(self):
-        """
-        Return a table object to use. The table has automatic support for
-        sorting and pagination.
-        """
-        if self.request.GET.get('datatable', False):
-            return simplejson.loads(self.request.GET['datatable'])
-        else:
-            return self.datatable.initial
+    # def get_tranformation_params(self):
+    #     """
+    #     Return a table object to use. The table has automatic support for
+    #     sorting and pagination.
+    #     """
+    #     if self.request.GET.get('datatable', False):
+    #         return simplejson.loads(self.request.GET['datatable'])
+    #     else:
+    #         return self.datatable._state
 
     def get_queryset(self):
         """
         Return the datatable queryset class, transformed appropriately.
         """
         if self.datatable is not None:
-            datatable = self.datatable
+            datatable_instance = self.datatable()
         # elif self.model is not None:
         #     datatable = default_datatable(self.model).all()
         else:
@@ -37,11 +37,10 @@ class DatatableMixin(object):
                                        u"%(cls)s.datatable"
                                        % {"cls": type(self).__name__})
         # Give datatable id so it can be referenced in html DOM elements
-        datatable.id = self.get_context_object_name(datatable)
-        datatable.state = simplejson.dumps(self.get_tranformation_params())
-        return datatable.all().transform(**self.get_tranformation_params())
+        datatable_instance.id = self.get_context_object_name(datatable_instance)
+        return datatable_instance
 
-    def get_context_object_name(self, table):
+    def get_context_object_name(self, queryset):
         """
         Get the name to use for the table's template variable.
         If not provided, use underscored version of datatable class name
@@ -49,10 +48,13 @@ class DatatableMixin(object):
         if self.context_datatable_name:
             context_datatable_name = self.context_datatable_name
         else:
-            context_datatable_name = re.sub('(((?<=[a-z])[A-Z])|([A-Z](?![A-Z]|$)))', '_\\1', table.model.__name__).lower().strip('_') + "_table"
+            if hasattr(queryset,"object_list"):
+                model = queryset.object_list.model
+            else:
+                model = queryset.model
+            context_datatable_name = re.sub('(((?<=[a-z])[A-Z])|([A-Z](?![A-Z]|$)))', '_\\1', model.__name__).lower().strip('_') + "_table"
         return context_datatable_name
-    
-    
+        
     def get_context_data(self, **kwargs):
         """
         Get the context for this view.
@@ -83,7 +85,32 @@ class DatatableMixin(object):
         # return super(DatatableMixin, self).get_context_data(**context)
        
 class DatatableView(DatatableMixin, ListView):
-   """
-   Generic view that renders a template and passes in a ``Datatable`` object.
-   """
-   pass
+    """
+    Generic view that renders a template and passes in a ``Datatable`` object.
+    """
+    def get(self, request, *args, **kwargs):
+        # Inject untransformed datatable into session if not there already
+        if not hasattr(request.session, 'datatable'):
+            request.session['datatable'] = self.get_queryset()
+        # If recieving params, update datatable state
+        print request.session['datatable']
+        if request.GET:
+            request.session['datatable'] = request.session['datatable'].update_state(**request.GET)
+        self.object_list = request.session['datatable']
+        
+        allow_empty = self.get_allow_empty()
+
+        if not allow_empty:
+            # When pagination is enabled and object_list is a queryset,
+            # it's better to do a cheap query than to load the unpaginated
+            # queryset in memory.
+            if (self.get_paginate_by(self.object_list) is not None
+                and hasattr(self.object_list, 'exists')):
+                is_empty = not self.object_list.exists()
+            else:
+                is_empty = len(self.object_list) == 0
+            if is_empty:
+                raise Http404(_(u"Empty list and '%(class_name)s.allow_empty' is False.")
+                        % {'class_name': self.__class__.__name__})
+        context = self.get_context_data(object_list=self.object_list)
+        return self.render_to_response(context)
